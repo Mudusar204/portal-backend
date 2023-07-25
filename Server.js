c97a00da6b916b12e5621780b986420a5f16e068
Mudusar204 <hafizmudusar@example.com>
2023-07-25 21:48:19 +0500

    2nd commit

--- Server.js
+++ Server.js
@@ -5,7 +5,7 @@ const port = 8000;
 const db = require("./src/config/dbConnection");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-
+// const multer = require('multer');
 const addStudent=require("./src/routes/studentRoutes")
 const users = require("./src/routes/userRoutes");
 
@@ -15,6 +15,28 @@ db.dbConnection();
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+////////////////////////multer////////////////////////////
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, './src/assets/images');
+//   },
+//   filename: function (req, file, cb) {
+//     const uniqueSuffix = Math.round(Math.random() * 1000)
+//     cb(null, file.fieldname + '-' + uniqueSuffix+".png");
+//   },
+// });
+
+// const upload = multer({storage });
+
+// app.post('/upload', upload.single('file'), async(req, res) => {
+//   if (!req.file) {
+//     return res.status(400).json({ error: 'No file received' });
+//   }
+ 
+//   res.json({ message: 'File uploaded successfully' });
+// });
+
 
 ///////////////////////////routes///////////////////////////////
 app.use("/students", addStudent);
--- src/controller/studentController.js
+++ src/controller/studentController.js
@@ -1,46 +1,71 @@
 const { studentModel } = require("../models/studentModel");
+const multer = require("multer");
+const uploadFile = require("../utils/uploadFile");
+// const uploadFile = require("../assets/images/");
 
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./src/assets/images");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Math.round(Math.random() * 1000);
+    cb(null, file.fieldname + "-" + uniqueSuffix + ".png");
+  },
+});
+
+const upload = multer({ storage });
 const addStudent = async (req, res) => {
   try {
-    const students=await studentModel.find()
-    console.log(students.length);
-    const student = new studentModel({
-      studentCode:students.length+1 ,
-      name: req.body.name,
-      fatherName: req.body.fatherName,
-      cnic: req.body.cnic,
-      email: req.body.email,
-      phone: req.body.phone,
-      class: req.body.class,
-      status: req.body.status,
-      qualification:req.body.qualification,
-      address:req.body.address,
-      dob:req.body.dob
-    });
-    await student.save();
-    console.log("added student");
-    res.send("added");
+    upload.single("file"),
+      async (req, res) => {
+        if (!req.file) {
+          return res.status(400).json({ error: "No file received" });
+        }
+        let { status, data } = await uploadFile(
+          "name",
+          "interviewQuestions",
+          "../assets/images/file-285.png"
+        );
+        console.log(status, data, "file uploaded");
+        const students = await studentModel.find();
+        console.log(students.length);
+        const student = new studentModel({
+          studentCode: students.length + 1,
+          name: req.body.name,
+          fatherName: req.body.fatherName,
+          cnic: req.body.cnic,
+          email: req.body.email,
+          phone: req.body.phone,
+          class: req.body.class,
+          status: req.body.status,
+          qualification: req.body.qualification,
+          address: req.body.address,
+          dob: req.body.dob,
+        });
+        await student.save();
+        console.log("added student");
+        res.send("added");
+      };
   } catch (error) {
     console.log(error);
-    res.send(error)
+    res.send(error);
   }
- 
 };
 
-const getStudents=async(req,res)=>{
+const getStudents = async (req, res) => {
   try {
-    const students=await studentModel.find()
+    const students = await studentModel.find();
     console.log(students);
     res.json({
-      message:"success",
-      students
-    })
+      message: "success",
+      students,
+    });
   } catch (error) {
     console.log(error);
     res.json({
-      message:"error"
-    })
+      message: "error",
+    });
   }
-}
+};
 
-module.exports = { addStudent ,getStudents};
+module.exports = { addStudent, getStudents };
